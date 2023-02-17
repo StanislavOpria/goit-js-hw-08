914c87c1de2e3eaf531b0906c80823383a883604
Shippuden90 <stasvk90@gmail.com>
2023-02-17 15:47:14 +0100

    Made 1 and 2 ex

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const itemClasses = {
+  a: 'gallery__item',
+  img: 'gallery__image',
+};
+
+const galleryMarkup = galleryItems
+  .map(({ original, preview, description }) => {
+    return `
+    <a class="${itemClasses.a}" href="${original}">
+    <img class="${itemClasses.img}" src="${preview}" alt="${description}"/></a>
+    `;
+  })
+  .join('');
+
+gallery.innerHTML = galleryMarkup;
+
+let galleryLibrery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const saveInLocalStorage = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const loadFromLocalStorage = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+function getCurrentTime(event) {
+  const carrentTime = event.seconds;
+  saveInLocalStorage(LOCALSTORAGE_KEY, carrentTime);
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+}
+
+player.on('timeupdate', getCurrentTime);
+player.setCurrentTime(loadFromLocalStorage(LOCALSTORAGE_KEY));
